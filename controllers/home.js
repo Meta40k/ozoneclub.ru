bc6155992d4db291db6c8cca6f3712ff2ca8ff75
EnNov93 <ed135246@yandex.ru>
2020-12-30 15:07:48 +0300

    123

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,10 @@
-let app = new (require('express').Router)();
+let app = new(require('express').Router)();
 Database = require('arangojs').Database;
 const config = require('../config');
-
-// const db = new Database({ url: config.arangoUri });
-// db.useDatabase(config.db_name);
-// db.useBasicAuth(config.db_user, config.db_password);
+let nodemailer = require('nodemailer')
+    // const db = new Database({ url: config.arangoUri });
+    // db.useDatabase(config.db_name);
+    // db.useBasicAuth(config.db_user, config.db_password);
 
 app.get('/', (req, res, next) => {
     res.render('index');
@@ -14,4 +14,47 @@ app.get('/news', (req, res, next) => {
     res.render('news.html');
 });
 
+app.post('/sendMail', (req, res, next) => {
+    console.log(req.body)
+    var transporter = nodemailer.createTransport({
+        service: 'yandex',
+        auth: {
+            user: 'zakaz@ozoneclub.ru',
+            pass: 'ozoneclub.ru!'
+        }
+    });
+
+    var mailOptions = {
+        from: 'zakaz@ozoneclub.ru',
+        to: '630127@mail.ru',
+        subject: 'Заказ с сайта ozoneclub.ru',
+        text: ` игра: '${req.body.type}', имя: '${req.body.name}' ,  почта: '${req.body.email}' ,  телефон:  '${req.body.tel}',  коммент:  '${req.body.comment}'`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    var mailOptions = {
+        from: 'zakaz@ozoneclub.ru',
+        to: 'dstsarev@mail.ru',
+        subject: 'Заказ с сайта ozoneclub.ru',
+        text: ` игра: '${req.body.type}', имя: '${req.body.name}' ,  почта: '${req.body.email}' ,  телефон:  '${req.body.tel}',  коммент:  '${req.body.comment}'`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+            res.json({ done: false, error: error })
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.json({ done: true })
+        }
+    });
+
+
+})
 module.exports = app;
---
+++ public/js/sendForm.js
@@ -0,0 +1,32 @@
+function sendMail() {
+
+    let data = {}
+
+    data.type = document.getElementById('inputGroupSelect01').value
+    data.name = document.getElementById('userName').value
+    data.email = document.getElementById('userEmail').value
+    data.tel = document.getElementById('userTel').value
+    data.comment = document.getElementById('userComment').value
+
+    if (data.name.length < 3 || data.name.email < 5) {
+        document.getElementById('alert').hidden = false
+        setTimeout(() => { document.getElementById('alert').hidden = true }, 3000)
+    } else {
+        console.log(data)
+        $.post('/sendMail', data).then((res) => {
+            if (res.done) {
+                document.getElementById('alertS').hidden = false
+                    // setTimeout(() => { document.getElementById('alertS').hidden = true }, 3000)
+                document.getElementById('inputGroupSelect01').disabled = true
+                document.getElementById('userName').disabled = true
+                document.getElementById('userEmail').disabled = true
+                document.getElementById('userTel').disabled = true
+                document.getElementById('userComment').disabled = true
+                document.getElementById('btnS').disabled = true
+
+                $('#btclose').click()
+            }
+        })
+    }
+
+}
